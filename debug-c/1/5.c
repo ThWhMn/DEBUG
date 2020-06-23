6652b0f8006033bbbfacbf4dcdb8959dab767882
keepcalm926 <1294872859@qq.com>
2020-06-23 21:55:53 +0800

    From xiao

--- debug-c/1/5.c
+++ debug-c/1/5.c
@@ -4,7 +4,7 @@
 
 double bisection(int p, int q, double (*func)(int, int, double));
 double f(int p, int q, double x);
-int main() {
+int main(){
     int p;
     int q;
     scanf("%d%d", &p, &q);
@@ -12,24 +12,24 @@ int main() {
     return 0;
 }
 
-double bisection(int p, int q, double (*func)(int, int, double)) {    double a;
+double bisection(int p, int q, double (*func)(int, int, double)){
+    double a;
     double b;
     double c;
     a = 20;
     b = -20;
-    c = (a + b)/ 2;
-    while(fabs(f(p,q,c)) >= EPSILON){
-        if((f(p,q,a) * f(p,q,c)) < 0) {
-            c = (c + a)/ 2, b = c,c = (a + b)/2;
-        }
-        if((f(p,q,b) * f(p,q,c)) < 0) {
-            c = (b + c)/ 2, a = c,c = (a + b)/2;
+    c = (a + b) / 2;
+    while (fabs(f(p, q, c)) >= EPSILON){/*which can make c approach answer*/
+        if ((f(p, q, a) * f(p, q, c)) < 0){/*0 between a and c*/
+            b = c, c = (a + b) / 2;
+        } 
+        if ((f(p, q, b) * f(p, q, c)) < 0){/*0 between b and c*/
+            a = c, c = (a + b) / 2;
         }
     }
     return c;
-        
 }
 
-double f(int p, int q, double x) {
+double f(int p, int q, double x){/*calcalate value*/
     return p * x + q;
 }
