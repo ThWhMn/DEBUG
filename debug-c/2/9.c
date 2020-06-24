08e3be47fb749880f2771e5620e26133e5b44a30
keepcalm926 <1294872859@qq.com>
2020-06-24 18:10:08 +0800

    second

--- debug-c/2/9.c
+++ debug-c/2/9.c
@@ -1,62 +1,48 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main() {
+int main(){
 	int matrix_a[10][10];
 	int matrix_b[10][10];
 	int matrix_c[10][10];
 	int m;
 	int n;
-	
-	//Ö÷Ñ­»·²ÎÊı
-	int i, j, k;
-	
-	//³Ë»ı¾ØÕó³õÊ¼»¯
-	for (i = 0; i <= 10; i++){
+	int i, j, k; //ä¸»å¾ªç¯å‚æ•°
+	for (i = 0; i <= 10; i++){//ä¹˜ç§¯çŸ©é˜µåˆå§‹åŒ–
 		for (k = 0; k <= 10; k++){
 			matrix_c[i][k] = 0;
 		}
-		
 	}
-	
-	//m¡¢nÊäÈë
-	scanf("%d %d", &m, &n);
-	
-	//¾ØÕóÊäÈë
-	for (i = 0; i < m; i++){
+	scanf("%d %d", &m, &n);//mã€nè¾“å…¥
+	for (i = 0; i < m; i++){//çŸ©é˜µè¾“å…¥
 		for (k = 0; k < n; k++){
 			scanf("%d", &matrix_a[i][k]);
-		}	
+		}
 	}
-			
-		
+
 	for (k = 0; k < n; k++){
 		for (i = 0; i < m; i++){
 			scanf("%d", &matrix_b[k][i]);
 		}
 	}
-	
-	//³Ë·¨
-	for (i = 0; i < m; i++){
-		for(j = 0; j < m; j++){
+	for (i = 0; i < m; i++){//ä¹˜æ³•
+		for (j = 0; j < m; j++){
 			for (k = 0; k < n; k++){
 				matrix_c[i][j] += (matrix_a[i][k] * matrix_b[k][j]);
 			}
 		}
 	}
-	
 	for (i = 0; i < m; i++){
-			for (k = 0; k < m; k++){
-				printf("%d", matrix_c[i][k]);
-				if (k < m - 1 ) {
-					printf(" ");
-				}
-				if (i == m - 1) {
-					continue;
-				}
-				if (k == m - 1) {
-					printf("\n");
-				}
+		for (k = 0; k < m; k++){
+			printf("%d", matrix_c[i][k]);
+			if (k < m - 1){
+				printf(" ");
+			} if (i == m - 1){
+				continue;
+			} if (k == m - 1)
+			{
+				printf("\n");
 			}
 		}
+	}
 	return 0;
 }
