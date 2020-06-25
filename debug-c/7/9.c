d33b3f5c3ec235a482905e6c87eddf113f8a910b
keepcalm926 <1294872859@qq.com>
2020-06-25 23:50:50 +0800

    7.9.c

--- debug-c/7/9.c
+++ debug-c/7/9.c
@@ -3,61 +3,46 @@ int main()
 {
     int y, m, d;
     scanf("%d-%d-%d", &y, &m, &d);
-    if (m == 2)
-    {
-        if (y % 4 == 0)
-        {
-            if (y % 100 == 0 && y % 400 != 0)
-            {
-                if (d <= 28 && d >= 1)
-                {
+    if (m == 2) {
+        if (y % 4 == 0) {
+            if (y % 100 == 0 && y % 400 != 0) {
+                if (d <= 28 && d >= 1) {
                     printf("YES");
-                }
-                else
-                {
+                } else {
                     printf("NO");
                 }
             }
-            else
-            {
-                if (d <= 29 && d >= 1)
-                {
+            else {
+                if (d <= 29 && d >= 1) {
                     printf("YES");
-                }
-                else
-                {
+                } else {
                     printf("NO");
                 }
             }
         }
-        else
-        {
-            if (d <= 28 && d >= 1)
-            {
+        else {
+            if (d <= 28 && d >= 1) {
                 printf("YES");
-            }
-            else
-            {
+            } else {
                 printf("NO");
             }
         }
     }
-    else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-    {
-        if (d >= 1 && d <= 31)
+    else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12){
+        if (d >= 1 && d <= 31) {
             printf("YES");
-        else
+        } else {
             printf("NO");
+        }
     }
-    else if (m == 4 || m == 6 || m == 9 || m == 11)
-    {
-        if (d >= 1 && d <= 30)
+    else if (m == 4 || m == 6 || m == 9 || m == 11) {
+        if (d >= 1 && d <= 30) {
             printf("YES");
-        else
+        } else {
             printf("NO");
+        }
     }
-    else
-    {
+    else {
         printf("NO");
     }
     return 0;
