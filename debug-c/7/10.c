7db49ba31fee49df7996c44b18fdf9ffd7666e1a
ThWhMn <1692657550@qq.com>
2020-06-25 20:02:20 +0800

    solve 7th question

--- debug-c/7/10.c
+++ debug-c/7/10.c
@@ -1,24 +1,47 @@
-#include<stdio.h>
-#include<math.h>
-int main() {
-    int a,b,c;
- scanf("%d%d%d",&a,&b,&c);
-    if((b<1)||(b>12)){printf("NO");}
-    else if((b==1)&&((c<1)||(c>31))){printf("NO");}
-     else if((b==3)&&((c<1)||(c>31))){printf("NO");}
-     else if((b==4)&&((c<1)||(c>30))){printf("NO");}
-     else if((b==5)&&((c<1)||(c>31))){printf("NO");}
-     else if((b==6)&&((c<1)||(c>30))){printf("NO");}
-     else if((b==7)&&((c<1)||(c>31))){printf("NO");}
-     else if((b==8)&&((c<1)||(c>31))){printf("NO");}
-    else if((b==9)&&((c<1)||(c>30))){printf("NO");}
-    else if((b==10)&&((c<1)||(c>31))){printf("NO");}
-    else if((b==11)&&((c<1)||(c>30))){printf("NO");}
-    else if((b==12)&&((c<1)||(c>31))){printf("NO");}
-    else if((a%400==0)&&b==2&&(c<1||c>29)){printf("NO");}
-    else if((a%100==0||a%400!=0)&&b==2&&(c<1||c>28)){printf("NO");}
-     else if((a%4==0||a%100!=0)&&b==2&&(c<1||c>29)){printf("NO");}
-else {printf("YES");}
-    
-    return 0;    
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d-%d-%d", &a, &b, &c);
+    if ((b < 1) || (b > 12)) { // invalid month number
+        printf("NO");
+    } else if ((b == 1) && ((c < 1) || (c > 31))) { // January
+        printf("NO");
+    } else if ((b == 3) && ((c < 1) || (c > 31))) { // March
+        printf("NO");
+    } else if ((b == 4) && ((c < 1) || (c > 30))) { // April
+        printf("NO");
+    } else if ((b == 5) && ((c < 1) || (c > 31))) { // May
+        printf("NO");
+    } else if ((b == 6) && ((c < 1) || (c > 30))) { // June
+        printf("NO");
+    } else if ((b == 7) && ((c < 1) || (c > 31))) { // July
+        printf("NO");
+    } else if ((b == 8) && ((c < 1) || (c > 31))) { // August
+        printf("NO");
+    } else if ((b == 9) && ((c < 1) || (c > 30))) { // September
+        printf("NO");
+    } else if ((b == 10) && ((c < 1) || (c > 31))) { // October
+        printf("NO");
+    } else if ((b == 11) && ((c < 1) || (c > 30))) { // November
+        printf("NO");
+    } else if ((b == 12) && ((c < 1) || (c > 31))) { // December
+        printf("NO");
+    } else if ((a % 400 == 0) && b == 2 && (c < 1 || c > 29)) {
+        // February in leap year
+        printf("NO");
+    } else if ((a % 100 != 0 || a % 4 == 0) && b == 2 && (c < 1 || c > 29)) {
+        // February in leap year
+        printf("NO");
+    } else if ((b == 2 && (c < 1 || c > 28))) {
+        // February in Non-leap year
+        printf("NO");
+    } else {
+        printf("YES");
+    }
+
+    return 0;
 }
