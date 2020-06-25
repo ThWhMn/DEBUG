af43a82de95e60a195cef4fdd2ec7cfd3f33f429
ThWhMn <52445096+ThWhMn@users.noreply.github.com>
2020-06-25 21:49:00 +0800

    Update 3.c

--- debug-c/7/3.c
+++ debug-c/7/3.c
@@ -3,26 +3,21 @@ int main()
 {
     int a, b, c;
     scanf("%d-%d-%d", &a, &b, &c);
-    if (b > 12 || c > 31) {//month>12 day>31 error
+    if (b > 12 || c > 31) { // month>12 day>31 error
         printf("NO");
-    }
-    else if (b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12) {
+    } else if (b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12) {
         if (c <= 31) {
             printf("YES");
-        }
-        else {
+        } else {
             printf("NO");
         }
-    }
-    else if (b == 4 || b == 6 || b == 9 || b == 11) {
+    } else if (b == 4 || b == 6 || b == 9 || b == 11) {
         if (c > 30) {
             printf("NO");
-        }
-        else {
+        } else {
             printf("YES");
         }
-    }
-    else if (b == 2) {
+    } else if (b == 2) {
         if (a % 4 == 0) {
             if (a % 100 == 0 && a % 400 != 0) {
                 if (c <= 28) {
@@ -44,8 +39,7 @@ int main()
                 printf("NO");
             }
         }
-    }
-    else {
+    } else {
         printf("NO");
     }
     return 0;
