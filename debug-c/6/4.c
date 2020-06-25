ed8b337efa07ff5781de9ad66edb0213779e6a0e
ThWhMn <1692657550@qq.com>
2020-06-25 17:44:35 +0800

    solve 6th question

--- debug-c/6/4.c
+++ debug-c/6/4.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-	char before[100];
-    scanf("%s",before);
+
+int main()
+{
+    char before[100];
     char calculate[100][100];
-    int i;
-    int count=0;
-    while(scanf("%s",calculate[i])!=EOF){
+    int  i;
+    int  count  = 0;
+    int  result = 0;
+
+    scanf("%s", before);                         // input in initial permissions
+    while (scanf("%s", calculate[i++]) != EOF) { // input permission changes
         count++;
     }
-    int result=0;
-    for(i=0;i<3;i++){
-        if(before[i]=='r') result+=4;
-        if(before[i]=='w') result+=2;
-        if(before[i]=='x') result+=1;
+
+    for (i = 0; i < 3; i++) { // calculate the initial value
+        if (before[i] == 'r')
+            result += 4;
+        if (before[i] == 'w')
+            result += 2;
+        if (before[i] == 'x')
+            result += 1;
     }
-    //计算后的结果
-    for(i=0;i<count;i++){
-    	if(calculate[i][0]=='+'){
-            if(calculate[i][1]=='r') result+=4;
-            if(calculate[i][1]=='w') result+=2;
-            if(calculate[i][1]=='x') result+=1;
-        }else if(calculate[i][0]=='-'){
-            if(calculate[i][1]=='r') result-=4;
-            if(calculate[i][1]=='w') result-=2;
-            if(calculate[i][1]=='x') result-=1;
-        }    
+
+    for (i = 0; i < count; i++) { // perform calculations on authority values
+        if (calculate[i][0] == '+') {
+            if (calculate[i][1] == 'r')
+                result += 4;
+            if (calculate[i][1] == 'w')
+                result += 2;
+            if (calculate[i][1] == 'x')
+                result += 1;
+        } else if (calculate[i][0] == '-') {
+            if (calculate[i][1] == 'r')
+                result -= 4;
+            if (calculate[i][1] == 'w')
+                result -= 2;
+            if (calculate[i][1] == 'x')
+                result -= 1;
+        }
     }
-    
-    printf("%d",result);
+    printf("%d", result);
     return 0;
 }
--- debug-c/6/8.c
+++ debug-c/6/8.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
-int main() {
-    char n;
-    int a[3]={0,0,0};
-    for(int i=0;i<3;i++){
-       scanf("%c",&n);
-        if(n == 'r'){
-            a[0]=1;
+
+int main()
+{
+    char n, o;
+    int  a[3] = {0, 0, 0};
+
+    for (int i = 0; i < 3; i++) { // initialize a[]
+        scanf("%c", &n);
+        if (n == '\n') {
+            break;
         }
-        if(n == 'w'){
-            a[1]=1;
+        if (n == 'r') {
+            a[0] = 1;
         }
-        if(n == 'x'){
-            a[2]=1;
+        if (n == 'w') {
+            a[1] = 1;
         }
-        
+        if (n == 'x') {
+            a[2] = 1;
+        }
     }
-    while (scanf("%c" , &n) !=EOF){
-       if(n == '\n'){
-          continue;
-       }
-       if(n=='+'){
-           scanf("%c" , &n);
-           if(n == 'r'){
-            a[0]=1;
-           }
-           if(n == 'w'){
-            a[1]=1;
-           }
-           if(n == 'x'){
-            a[2]=1;
-           }  
-       } 
-        if(n=='-'){
-           scanf("%c" , &n);
-           if(n == 'r'){
-            a[0]=0;
-           }
-           if(n == 'w'){
-            a[1]=0;
-           }
-           if(n == 'x'){
-            a[2]=0;
-           }  
-       } 
+    while (scanf("%c", &n) != EOF) {
+        if (n == '\n') {
+            continue;
+        }
+        if (n == '+') {      // read operator
+            scanf("%c", &n); // read authority sign
+            if (n == 'r') {
+                a[0] = 1;
+            }
+            if (n == 'w') {
+                a[1] = 1;
+            }
+            if (n == 'x') {
+                a[2] = 1;
+            }
+        }
+        if (n == '-') {
+            scanf("%c", &n);
+            if (n == 'r') {
+                a[0] = 0;
+            }
+            if (n == 'w') {
+                a[1] = 0;
+            }
+            if (n == 'x') {
+                a[2] = 0;
+            }
+        }
     }
-    int o=a[0]*2*2+a[1]*2+a[2];
-    printf("%d",o);
+    o = a[0] * 4 + a[1] * 2 + a[2];
+    printf("%d", o);
     return 0;
 }
