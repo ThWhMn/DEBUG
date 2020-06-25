05d992ec09a9bd6aca5f634d8ad002e397720ea2
ThWhMn <52445096+ThWhMn@users.noreply.github.com>
2020-06-25 21:48:15 +0800

    Update 1.c

--- debug-c/7/1.c
+++ debug-c/7/1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main()
 {
     int year;
@@ -12,50 +13,36 @@ int main()
             } else {
                 printf("NO");
             }
-        }
-        else if (mouth == 4 || mouth == 6 || mouth == 9 || mouth == 11) {
-            if (day <= 30 & day >= 1){
+        } else if (mouth == 4 || mouth == 6 || mouth == 9 || mouth == 11) {
+            if (day <= 30 & day >= 1) {
                 printf("YES");
             } else {
                 printf("NO");
             }
-        }
-        else if (mouth == 2) {
+        } else if (mouth == 2) {
             if (year % 4 == 0) {
                 if (year % 100 == 0 && year % 400 != 0) {
                     if (day <= 28 && day >= 1) {
                         printf("YES");
-                    } else{
+                    } else {
                         printf("NO");
                     }
-                }
-                else
-                {
-                    if(day <= 29 && day >= 1)
-                    {
+                } else {
+                    if (day <= 29 && day >= 1) {
                         printf("YES");
-                    }
-                    else
-                    {
+                    } else {
                         printf("NO");
                     }
                 }
-            }
-            else
-            {
-                if (day <= 28 && day >= 1)
-                {
+            } else {
+                if (day <= 28 && day >= 1) {
                     printf("YES");
-                }
-                else
-                {
+                } else {
                     printf("NO");
                 }
             }
         }
-    }
-    else
-    {
+    } else {
         printf("NO");
     }
     return 0;
