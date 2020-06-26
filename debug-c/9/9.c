4f3e29cc3ab3cff77419eb97f03786727b0f4f60
keepcalm926 <1294872859@qq.com>
2020-06-26 12:40:45 +0800

    ninth

--- debug-c/9/9.c
+++ debug-c/9/9.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n;
-    scanf("%d",&n);
-    
+    scanf("%d", &n); //input
     int a[51];
-    if(n==2){
+    if (n == 2) {
         printf("1");
-    }
-    if(n==3){
+    } if (n == 3) {
         printf("1");
-    }if(n>3){
-    a[2]=1;
-    a[3]=1;
-    a[1]=0;
+    } if (n > 3) {
+        a[2] = 1;
+        a[3] = 1;
+        a[1] = 0;
         int i;
-    for( i=4;i<=n;i++){
-        a[i]=a[i-2]+a[i-3];
-        
-    }
-        printf("%d",a[i]);
+        for (i = 4; i <= n; i++) {
+            a[i] = a[i - 2] + a[i - 3];
+        }
+        printf("%d", a[i-1]); //i=n+1 ouput
     }
     return 0;
 }
