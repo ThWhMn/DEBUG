9fb0003ee1065afb0719647a54614ea84cea27d8
keepcalm926 <1294872859@qq.com>
2020-06-26 17:39:35 +0800

    twelve

--- debug-c/12/9.c
+++ debug-c/12/9.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 #define PI 3.14159
-int main() {
-	float r;
-    float c, s;
-	scanf("%g", &r);
+
+int main()
+{
+    double r, c, s;
+    scanf("%lf", &r);
     c = 2 * PI * r;
-    s = PI * r *r;
-    printf("%.3f\n",c);
-    printf("%.3f\n",s);
+    s = PI * r * r;
+    printf("%.3f\n", c);
+    printf("%.3f", s);
     return 0;
 }
