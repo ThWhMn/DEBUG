eafe27d3f616c384ae46aaf718bfa8fddd046d30
ThWhMn <1692657550@qq.com>
2020-06-26 13:59:18 +0800

    solve 9th question

--- debug-c/9/2.c
+++ debug-c/9/2.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-int main() {
-	int N;//总台阶数
-    scanf("%d", &N);
-    int step[50];//step[N]N级台阶的方法数
+int main()
+{
+    int N;        //总台阶数
+    int step[50]; // step[N]N级台阶的方法数
     int i;
-    step[0] = 0;
-    step[1] = 0;
+
+    step[1] = 0; // initialize step[]
     step[2] = 1;
     step[3] = 1;
     step[4] = 1;
-    for (i = 5; i < N; i++){
+    scanf("%d", &N);
+    for (i = 5; i <= N; i++) {
         step[i] = step[i - 2] + step[i - 3];
     }
+
     printf("%d", step[N]);
     return 0;
 }
