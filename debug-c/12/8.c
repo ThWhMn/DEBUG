b2f2132c1dcdbcab610d3a9c1c968237886cce76
ThWhMn <1692657550@qq.com>
2020-06-26 16:23:26 +0800

    solve 12th question "It's so easy :)"

--- debug-c/12/8.c
+++ debug-c/12/8.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#define PI 3.14159
-int main() {
-    double r;
-    scanf("%lf",r);
-    printf("%.3f\n",2*PI*r);
-    printf("%.3f",PI*r*r);
 
+#define PI 3.14159
+
+int main()
+{
+    double r;
+
+    scanf("%lf", &r);
+    printf("%.3lf\n", 2 * PI * r);
+    printf("%.3lf", PI * r * r);
 
     return 0;
 }
