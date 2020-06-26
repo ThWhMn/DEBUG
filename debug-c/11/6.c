6867b72e7c1d1f22ef5a639d6fc2b602f7790fd2
ThWhMn <1692657550@qq.com>
2020-06-26 16:16:08 +0800

    solve 11th question

--- debug-c/11/6.c
+++ debug-c/11/6.c
@@ -1,39 +1,33 @@
-
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char arr[32];
-    char str[32];
-    int bits[32];
-    char result[32];
-    
-    scanf("%s", &arr[32]);
+int main()
+{
+    char arr[32], str[32], result[32];
+    int  bits[32], b;
+    int  j = 0;
     char input;
-    int j = 0;
-    
-    for(int i = 0; i < 32; i++){
-        arr[i] = '0';
+
+    for (int i = 0; i < 32; i++) { // initialize arr[]
+        arr[i] = 0;
     }
-    
+
     while (scanf("%c", &input) != EOF) {
-        if (input == '\n'){
-            break;
-        }else if(j >= 32){
+        if (input == '\n') {
             break;
         }
-        arr[j % 32] = arr[j % 32] + (int)(input);
         j++;
+        arr[j % 32] += input;
     }
-   
+
     for (int i = 0; i < 32; i++) {
-        bits[i] = (arr[31 - i]) ^ (arr[i] << 1);
-        int b = (bits[i] % 85) + 34;
-        result[i] = (char)b;
+        bits[i]       = (arr[31 - i]) ^ (arr[i] << 1);
+        b             = (bits[i] % 85) + 34;
+        result[i + 1] = b;
     }
-    
-    for (int i = 0; i < 32; i++) {
-        printf("%c",result[i]);
+
+    for (int i = 1; i < 33; i++) {
+        printf("%c", result[i]);
     }
     return 0;
 }
--- debug-c/11/8.c
+++ debug-c/11/8.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void f(char []);
+void f(char[]);
 
 int main()
 {
     char string[500];
-    
+
     scanf("%s", string);
     f(string);
     return 0;
@@ -14,20 +14,22 @@ int main()
 
 void f(char string[])
 {
-    int arr[32], bits[32];
+    int  arr[32], bits[32];
     char result[33];
-    int i, j;
-    unsigned long k;
-    
-    for (i = 0; i < 32; i++)
+    int  i, j, k;
+
+    for (i = 0; i < 32; i++) // initialize arr[]
         arr[i] = 0;
+
     k = strlen(string);
     for (i = 1; i <= k; i++)
-        arr[i % 32] += (int)string[i - 1];
-    for (j = 0; j < 32; j++){
-        bits[j] = arr[31 - j] ^ (arr[j] << 1);
-        result[j + 1] = (char)(bits[j] % 85 + 34);
+        arr[i % 32] += string[i - 1];
+
+    for (j = 0; j < 32; j++) {
+        bits[j]       = arr[31 - j] ^ (arr[j] << 1);
+        result[j + 1] = bits[j] % 85 + 34;
     }
-    for (j = 0; j < 33; j++)
+
+    for (j = 1; j < 33; j++)
         printf("%c", result[j]);
 }
