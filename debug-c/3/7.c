d59d72c9d340dbf35e3d3f1173a6a80b2c066a5c
ThWhMn <1692657550@qq.com>
2020-06-27 16:18:24 +0800

    solve 3rd question:3, 5, 7, 9, 10

--- debug-c/3/7.c
+++ debug-c/3/7.c
@@ -1,75 +1,72 @@
 #include <stdio.h>
-int main() {
+
+int main()
+{
     int matrix[100][100];
-    int m;
-    int n;
-    int i;
-    int j;
-    int startX;
-    int startY;
-    int endX;
-    int endY;
-   
-    startX = 0;
-    startY = 0;
-    endX = m;
-    endY = n;
-   
+    int m, n, i, j;
+    int startX, startY;
+    int endX, endY;
+    int count = 0;
+
     //输入行数m，列数n
     scanf("%d %d", &m, &n);
     // 输入一个m*n的矩阵
-    for (i = 0; i < m; i++){
-        for (j = 0; j < n; j++){
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-   
+
     // 判断循环圈数的条件
-    while (startX * 2 < m && startY * 2 < n) {
-        endX = m - 1 - startX;
-        endY = n - 1 - startY;
+    startX = 0;
+    startY = 0;
+    endX   = m - 1;
+    endY   = n - 1;
+    while (count != m * n) {
         // 输出第一步
-        if (startY < endY) {
+        if (startY <= endY && count != m * n) {
             for (j = startY; j <= endY; j++) {
                 printf("%d", matrix[startX][j]);
-                if (j <= endY) {
+                count++;
+                if (count != m * n) {
                     printf(" ");
                 }
             }
         }
-       
-          
         //满足以下条件输出第二步
-        if (startX < endX) {
+        if (startX <= endX && count != m * n) {
             for (j = startX + 1; j <= endX; j++) {
                 printf("%d", matrix[j][endY]);
-                if (j <= endY){
+                count++;
+                if (count != m * n) {
                     printf(" ");
                 }
             }
         }
         //满足以下条件输出第三步
-        if (endX > startX && startY < endY) {
-            for (j = endY - 1; j >= startY; j--){
+        if (endX >= startX && startY <= endY && count != m * n) {
+            for (j = endY - 1; j >= startY; j--) {
                 printf("%d", matrix[endX][j]);
-                if (j >= startY){
+                count++;
+                if (count != m * n) {
                     printf(" ");
                 }
             }
         }
         //满足以下条件输出第四步
-        if (endX - 1 > startX && startY < endY) {
-            for (i = endX - 1; i >= startX + 1; i--){
-                printf("%d ", matrix[i][startX]);
-                if (i >= startX + 1){
+        if (endX >= startX && startY <= endY && count != m * n) {
+            for (i = endX - 1; i >= startX + 1; i--) {
+                printf("%d", matrix[i][startX]);
+                count++;
+                if (count != m * n) {
                     printf(" ");
                 }
             }
         }
         startX++;
         startY++;
+        endX--;
+        endY--;
     }
-    
-   
     return 0;
 }
